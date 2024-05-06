0eefa1fde7d5ce314e0794ad995ad676ff356d51
BuriP <buriperez23@gmail.com>
2024-05-07 00:47:43 +0200

    Updated generated

--- src/generated/ID.go
+++ src/generated/ID.go
@@ -2,19 +2,25 @@ package generated
 
 import (
 	"regexp"
+	"fmt"
 
-	"github.com/google/uuid"
 )
-
-type ID string
-
-// Validate method checks if the ID has the required format.
-func (id ID) Validate() bool {
-	pattern := regexp.MustCompile(`^[a-zA-Z0-9\-_:]{2,64}$`) // Defining the expression for the pattern.
-	return pattern.MatchString(string(id))
+// ID represents an identifier expressed as a UUID.
+type ID struct {
+	Value string `json:"value" description:"An identifier expressed as a UUID"`
 }
 
-// IDFromString converts a string into an ID.
-func IDFromString(s string) ID {
-	return ID(s)
+// NewID creates a new instance of ID with the provided value.
+func NewID(value string) (*ID, error) {
+	// Validate the provided value against the regex pattern.
+	if !isValidID(value) {
+		return nil, fmt.Errorf("invalid ID format")
+	}
+	return &ID{Value: value}, nil
+}
+
+// isValidID checks if the provided value matches the regex pattern.
+func isValidID(value string) bool {
+	pattern := regexp.MustCompile(`^[a-zA-Z0-9\-_:]{2,64}$`)
+	return pattern.MatchString(value)
 }
