e26672a66c87fafee6d3262132300acbb206e2e2
BuriP <buriperez23@gmail.com>
2024-05-17 00:51:34 +0200

    gen update

--- src/generated/ID.go
+++ src/generated/ID.go
@@ -1,50 +1,37 @@
 package generated
 
 import (
-	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/google/uuid"
-	
 )
 
 // ID represents an identifier expressed as a UUID.
 type ID struct {
-	Value uuid.UUID `json:"value" description:"An identifier expressed as a UUID"`
-}
-
-// NewID creates a new instance of ID with the provided value.
-func NewID(value string) (*ID, error) {
-	validID, err := TransformToValidID(value)
-	if err!= nil {
-		return nil, err
-	}
-
-	if !isValidID(validID.String()){
-		return nil, fmt.Errorf("invalid Id Format")
-	}
-
-	return &ID{Value: validID}, nil
-}
-
-// isValidID checks if the provided value matches the regex pattern.
-func isValidID(value string) bool {
-	pattern := regexp.MustCompile(`^[a-zA-Z0-9\-_:]{2,64}$`)
-	return pattern.MatchString(value)
+	Value string `json:"value" description:"An identifier expressed as a UUID"`
 }
 
 // TransformToValidID transforms a string into a vlid ID type
-func TransformToValidID(value string) (uuid.UUID, error) {
+func TransformToValidID(value string) string {
 	// Here's a simple transformation that removes invalid characters:
 	re := regexp.MustCompile(`[^a-zA-Z0-9\-_:]{2,64}$`)
 	transformedValue := re.ReplaceAllString(value, "")
-	// Ensure the length is within the valid range (UUID length).
-	transformedValue = transformedValue[:min(len(transformedValue), 64)]
+	return transformedValue
+}
 
-	// Attempt to parse the transformed value as a UUID.
-	u, err := uuid.Parse(transformedValue)
+// CreateNewID creates a new ID following the specific pattern.
+func NewID() (*ID, error) {
+	// Generating a new random UUID
+	id, err := uuid.NewRandom()
 	if err != nil {
-		return uuid.Nil, fmt.Errorf("invalid UUID format: %v", err)
+		return nil, err
 	}
-	return u, nil
+
+	// Convert the UUID to string
+	idString := strings.ReplaceAll(id.String(), "-", "")
+
+	// Transform the string into a valid ID
+	validID := TransformToValidID(idString)
+	return &ID{Value: validID}, nil
 }
